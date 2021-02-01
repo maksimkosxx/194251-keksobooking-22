c2b03ce0f1db9e7e8f1cbda60172bfa1f5746266
maksimkosxx <maksimkosxx@gmail.com>
2021-02-01 09:39:46 +0300

    second function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,43 @@
 
-const randomInteger = (min, max) => {
+
+const getRandomInteger = (min, max) => {
   min = Math.ceil(min);
-  max = (Math.floor(max));
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  max = Math.floor(max);
+
+  if(min >= max){
+    alert('Значание min должно быть меньше max')
+    return undefined;
+
+  } else if(min < 0) {
+    alert('Значание min должно быть больше либо равно 0')
+    return undefined;
+
+  } else if(max < 0) {
+    alert('Значание max должно быть больше либо равно 0')
+    return undefined;
+
+  } else return Math.floor(Math.random() * (max - min + 1) + min);
+
+}
+
+const getRandomFractional = (min, max) => {
+
+  if(min >= max){
+    alert('Значание min должно быть меньше max')
+    return undefined;
+
+  } else if(min < 0) {
+    alert('Значание min должно быть больше либо равно 0')
+    return undefined;
+
+  } else if(max < 0) {
+    alert('Значание max должно быть больше либо равно 0')
+    return undefined;
+
+  } else return Math.random() * (max - min + 1) + min;
+
 }
 // eslint-disable-next-line no-console
-console.log(randomInteger(10, 5));
+console.log(getRandomInteger(0, 5));
+// eslint-disable-next-line no-console
+console.log(getRandomFractional(1.5, 5.5));
