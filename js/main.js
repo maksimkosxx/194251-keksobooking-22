080066f0f821fae0e516162ec2ac4ab2c1ee3650
maksimkosxx <maksimkosxx@gmail.com>
2021-02-02 17:50:43 +0300

    fix after review

--- js/main.js
+++ js/main.js
@@ -16,8 +16,9 @@ const getRandomInteger = (min, max) => {
     alert('Значение max должно быть больше либо равно 0')
     return undefined;
 
-  } else return Math.floor(Math.random() * (max - min + 1) + min);
-
+  } else {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 }
 
 const getRandomFractional = (min, max, signs) => {
@@ -36,10 +37,7 @@ const getRandomFractional = (min, max, signs) => {
     alert('Значение max должно быть больше либо равно 0')
     return undefined;
 
-  } else return result.toFixed(signs);
-
+  } else {
+    return result.toFixed(signs);
+  }
 }
-// eslint-disable-next-line no-console
-console.log(getRandomInteger(0, 5));
-// eslint-disable-next-line no-console
-console.log(getRandomFractional(1.5, 5.5, 3));
