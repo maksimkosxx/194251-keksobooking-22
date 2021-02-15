397167173a41fea94499cd7b46efee5bb85d3937
maksimkosxx <maksimkosxx@gmail.com>
2021-02-15 22:00:46 +0300

    module4 Разбиение на модули

---
+++ js/initData.js
@@ -0,0 +1,25 @@
+const PLACE = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+]
+const TIME = [
+  '12:00',
+  '13:00',
+  '14:00',
+]
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+]
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+]
+export {PLACE, TIME, FEATURES, PHOTOS}
